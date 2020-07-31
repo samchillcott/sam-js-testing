97b0f8ec0ecf3d473b67ae7207102270db3d6599
Sam Chillcott <sam@samchillcott.com>
2020-07-31 10:54:08 +0100

    headless test

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require("./util");
 
 test("should output name and age", () => {
@@ -8,4 +9,16 @@ test("should output name and age", () => {
 test("should generate a valid text output", () => {
     const text = checkAndGenerate( 'Sam', 41);
     expect(text).toBe('Sam (41 years old)');
+});
+
+test ('shoud click around', async() => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 80,
+        args: ['--window-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    await page.goto(
+        
+    )
 })
