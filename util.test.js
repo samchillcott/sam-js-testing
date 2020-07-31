38850f1994acc5759bb51f2766a6a5e7faf4b050
Sam Chillcott <sam@samchillcott.com>
2020-07-31 10:59:19 +0100

    newpage launch

--- util.test.js
+++ util.test.js
@@ -1,24 +1,24 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 const { generateText, checkAndGenerate } = require("./util");
 
 test("should output name and age", () => {
-    const text = generateText('Sam', 41);
-    expect(text).toBe('Sam (41 years old)');
+	const text = generateText("Sam", 41);
+	expect(text).toBe("Sam (41 years old)");
 });
 
 test("should generate a valid text output", () => {
-    const text = checkAndGenerate( 'Sam', 41);
-    expect(text).toBe('Sam (41 years old)');
+	const text = checkAndGenerate("Sam", 41);
+	expect(text).toBe("Sam (41 years old)");
 });
 
-test ('shoud click around', async() => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 80,
-        args: ['--window-size=1920,1080']
-    });
-    const page = await browser.newPage();
-    await page.goto(
-        
-    )
-})
+test("shoud click around", async () => {
+	const browser = await puppeteer.launch({
+		headless: false,
+		slowMo: 80,
+		args: ["--window-size=1920,1080"],
+	});
+	const page = await browser.newPage();
+	await page.goto(
+		"/Users/samchillcott/Desktop/DESK/CODE/GH Repos/js-testing-introduction/index.html"
+	);
+});
