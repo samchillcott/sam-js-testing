e968dc056e0f27a371f49d6c84b014449078202c
Sam Chillcott <sam@samchillcott.com>
2020-07-31 10:33:37 +0100

    updated exports.

--- util.test.js
+++ util.test.js
@@ -1,6 +1,11 @@
-const { generateText } = require("./util");
+const { generateText, checkAndGenerate } = require("./util");
 
 test("should output name and age", () => {
     const text = generateText('Sam', 41);
     expect(text).toBe('Sam (41 years old)');
 });
+
+test("should generate a valid text output", () => {
+    const text = checkAndGenerate( 'Sam', 41);
+    expect(text).toBe('Sam (41 years old)');
+})
